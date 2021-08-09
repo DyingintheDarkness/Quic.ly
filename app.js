03ada86c454cbb187703de2c29d9bd4175e22d22
DyingintheDarkness <vihaan.s310@gmail.com>
2021-08-09 18:41:32 +0530

    ''

--- app.js
+++ app.js
@@ -1,40 +1,71 @@
 const express = require("express");
 const app = express();
 const uniqid = require("uniqid");
+const path = require("path")
 const port = 5000;
+
+//engine setup
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.engine("html", require("ejs").renderFile)
+
+
+
 app.listen(port, (req, res) => {
   console.log(`Server is listening on port 5000.... 
 Localhost : http://localhost:${port}/
   `);
 });
 
+
+
+
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
   if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require("node-localstorage").LocalStorage;
     localStorage = new LocalStorage("./localStorage");
   }
+  res.render("index.html", {ids: "8282118",urls: "lol"});
   const url = req.query.url;
   if (url !== undefined) {
+    const id = uniqid();
+    let exists = false;
     if (localStorage) {
-      const id = uniqid();
-      localStorage.setItem(id, url);
+      for (let i = 0; i < localStorage._keys.length; i++) {
+        const localid = localStorage._keys[i];
+        const localurl = localStorage.getItem(localid);
+        if (localid !== id && localurl === url) {
+          exists = true;
+          break;
+        }
+      }
+      if (exists === false) {
+        localStorage.setItem(id, url);
+      }
     }
   }
   redirectLink(localStorage);
 });
 
 const redirectLink = (localStorage) => {
+  let urlExists = false
+  let url;
   app.get("/:id", (req, res) => {
     const id = req.params.id;
     const keys = localStorage._keys;
     for (let i = 0; i < keys.length; i++) {
-      const url = localStorage.getItem(keys[i]);
-      if (keys[i] === id) {
-        res.redirect(url);
-      } else {
-        res.sendFile("404.html", { root: __dirname });
+      const key = keys[i]
+      const storedurl = localStorage.getItem(key)
+      if(key === id){
+        urlExists = true;
+        url = storedurl
+        break;
       }
+    }
+    if (urlExists) {
+      res.redirect(url);
+    } else {
+      res.render("404.html");
     }
   });
 };
